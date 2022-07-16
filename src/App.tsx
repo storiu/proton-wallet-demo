5c95d30f9c42f341127c0cd57a85806744c26ed4
storiu <redcarrot0803@gmail.com>
2022-07-16 13:39:22 -0500

    feat: Implement `proton wallet connect`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import React from "react";
+import ProtonWebSDK from "@proton/web-sdk";
+
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const login = async () => {
+    await ProtonWebSDK({
+      linkOptions: {
+        endpoints: ["https://api-dev.protonchain.com/v1/chain/info"],
+      },
+      transportOptions: {},
+      selectorOptions: {
+        appName: "Tasklyy",
+        appLogo:
+          "https://taskly.protonchain.com/static/media/taskly-logo.ad0bfb0f.svg",
+        customStyleOptions: {
+          modalBackgroundColor: "#F4F7FA",
+          logoBackgroundColor: "white",
+          isLogoRound: true,
+          optionBackgroundColor: "white",
+          optionFontColor: "black",
+          primaryFontColor: "black",
+          secondaryFontColor: "#6B727F",
+          linkColor: "#752EEB",
+        },
+      },
+    });
+  };
+
+  return (
+    <>
+      <div onClick={login} className="text-3xl font-bold">
+        Wallet Connect
+      </div>
+    </>
+  );
 }
 
 export default App;
